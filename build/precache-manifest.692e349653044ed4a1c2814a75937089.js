e9d0f3ccda3068850611c48af0c1a7803afa6391
ioan.ghisoi95 <ionutghisoi95@gmail.com>
2020-07-16 12:50:24 +0100

    Add build

---
+++ build/precache-manifest.692e349653044ed4a1c2814a75937089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b653edd7f0f9db2e393f8613613b7fbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ce084b85d7276670a71",
+    "url": "/static/css/main.5880ca9a.chunk.css"
+  },
+  {
+    "revision": "46dc96668312c13d5584",
+    "url": "/static/js/2.b4c36f5e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b4c36f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce084b85d7276670a71",
+    "url": "/static/js/main.823c8f07.chunk.js"
+  },
+  {
+    "revision": "d286b1ef63976b1d3903",
+    "url": "/static/js/runtime-main.594f5b1c.js"
+  }
+]);
